a5777e79237feb6e8e9368562802b5708a18bb25
William Barcelos <william-barcelos011@hotmail.com>
2026-03-23 10:30:28 -0300

    Insert DepartmentDaoJDBC

---
+++ src/application/Program2.java
@@ -0,0 +1,22 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 4: Department insert =====");
+		Department newDepartment = new Department(null, "Oficina");
+		departmentDao.insert(newDepartment);
+		
+		System.out.println("Inserted! New id: " + newDepartment.getId());
+
+	}
+
+}
